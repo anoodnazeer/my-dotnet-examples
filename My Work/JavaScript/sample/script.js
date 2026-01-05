60786b2bc3848c1bbc658190957b30cb52f19bb0
anoodnazeer <anoodnazeer@gmail.com>
2026-01-05 20:33:56 +0400

    add work html css javascript bootstrap

---
+++ My Work/JavaScript/sample/script.js
@@ -0,0 +1,31 @@
+// alert("Welcome ");
+// console.log("This is the first messege");
+// document.write("This is my first programe");
+
+
+
+// alert("Hello! I am an alert box!!")
+
+// console.log("The alert()");
+
+// document.write("The alert box includes an OK button and is typically used to ensure that the user receives critical information or confirmation before proceeding.")
+// var x=5;
+// var x="ammu";
+// document.write("x");
+// document.write(x);
+// alert(x);
+// var y;
+
+// let x=5;
+//  x="ammu"
+// document.write(x);
+// const x=5;
+//  x="ammu";
+//  document.write(x);
+
+let x=3;
+let y=5;
+let z=0;
+z=x+y;
+document.write(z);
+alert(z)
